fae54026a6f94e6ffb03191b4c0c66226fc345d3
Matias <amatiascastillo@gmail.com>
2022-07-25 14:09:53 -0300

    commit

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { DataGrid, esES } from "@mui/x-data-grid";
-import { Paper } from "@mui/material";
+import { Paper, styled, TableCell, tableCellClasses } from "@mui/material";
 import { getAllIncriptions } from "../services";
 import { deepOrange } from "@mui/material/colors";
 import { esES as coreEsES } from "@mui/material/locale";
@@ -20,50 +20,155 @@ const theme = createTheme(
   coreEsES // core translations
 );
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#bf360c",
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
 const columns = [
   { field: "id", headerName: "#", width: 50 },
+  { field: "prov_asoc", headerName: "Asoc", width: 50 },
+  { field: "prov_dni", headerName: "DNI", width: 100 },
   {
-    field: "nombreTransportista",
-    headerName: "Nombre del transportista",
+    field: "prov_nombre",
+    headerName: "Apellido y Nombre Proveedor",
     width: 170,
   },
   {
-    field: "nombreColegio",
-    headerName: "Nombre del colegio",
+    field: "prov_titularVehiculo",
+    headerName: "Datos Titular Vehiculo",
     width: 200,
   },
-  { field: "direccColegio", headerName: "Dirección del colegio", width: 170 },
-  { field: "localidadColegio", headerName: "Localidad", width: 150 },
-  { field: "emailColegio", headerName: "Email del colegio", width: 250 },
-  { field: "telColegio", headerName: "Teléfono del colegio", width: 150 },
-  { field: "nombreDirectivo", headerName: "Nombre Directivo", width: 150 },
-  { field: "apellidoDirectivo", headerName: "Apellido Directivo", width: 150 },
+  { field: "chofer", headerName: "Choferes", width: 170 },
+  { field: "chofer_dni", headerName: "DNI", width: 150 },
+  { field: "chofer_patente", headerName: "Patente", width: 250 },
+  { field: "chofer_habilitacion", headerName: "Habilitacion", width: 150 },
+  { field: "chofer_vtoHab", headerName: "Vto Habilitacion", width: 150 },
+  { field: "chofer_seguro", headerName: "Seguro", width: 150 },
+  { field: "chofer_nPoliza", headerName: "Nº Poliza", width: 150 },
+  { field: "chofer_vtoPoliza", headerName: "Vto Poliza", width: 150 },
+  { field: "chofer_nVtv", headerName: "Nº VTV", width: 150 },
+  { field: "chofer_vtoVtv", headerName: "Vto VTV", width: 150 },
+  { field: "chofer_vehiculo", headerName: "Vehiculo", width: 150 },
+  { field: "chofer_vehiculoCapacidad", headerName: "Capacidad", width: 150 },
+  { field: "chofer_cupon", headerName: "Cupon de pago", width: 150 },
+  { field: "chofer_registro", headerName: "Vto Registro", width: 150 },
+  { field: "chofer_prorroga", headerName: "Prorroga al", width: 150 },
+  { field: "chofer_cuitSocio", headerName: "CUIT Socio", width: 150 },
   {
-    field: "dateViaje",
-    headerName: "Fecha del viaje",
-    width: 100,
-    align: "right",
+    field: "chofer_nombreTitular",
+    headerName: "Apellido y Nombre Titular",
+    width: 150,
   },
+  { field: "chofer_cuitTitular", headerName: "CUIT Titular", width: 150 },
+  { field: "chofer_anioMod", headerName: "Año Mod", width: 150 },
   {
-    field: "timeViaje",
-    headerName: "Hora del viaje",
-    width: 100,
-    align: "right",
+    field: "edit_delete",
+    headerName: "Editar/Borrar",
+    width: 150,
   },
 ];
 
-export default function DataTable() {
-  var [rows, setRows] = useState([]);
-  var [loading, setLoading] = useState(true);
+function createData(
+  id,
+  prov_asoc,
+  prov_dni,
+  prov_nombre,
+  prov_titularVehiculo,
+  chofer,
+  chofer_dni,
+  chofer_patente,
+  chofer_habilitacion,
+  chofer_vtoHab,
+  chofer_seguro,
+  chofer_nPoliza,
+  chofer_vtoPoliza,
+  chofer_nVtv,
+  chofer_vtoVtv,
+  chofer_vehiculo,
+  chofer_vehiculoCapacidad,
+  chofer_cupon,
+  chofer_registro,
+  chofer_prorroga,
+  chofer_cuitSocio,
+  chofer_nombreTitular,
+  chofer_cuitTitular,
+  chofer_anioMod
+) {
+  return {
+    id,
+    prov_asoc,
+    prov_dni,
+    prov_nombre,
+    prov_titularVehiculo,
+    chofer,
+    chofer_dni,
+    chofer_patente,
+    chofer_habilitacion,
+    chofer_vtoHab,
+    chofer_seguro,
+    chofer_nPoliza,
+    chofer_vtoPoliza,
+    chofer_nVtv,
+    chofer_vtoVtv,
+    chofer_vehiculo,
+    chofer_vehiculoCapacidad,
+    chofer_cupon,
+    chofer_registro,
+    chofer_prorroga,
+    chofer_cuitSocio,
+    chofer_nombreTitular,
+    chofer_cuitTitular,
+    chofer_anioMod,
+  };
+}
 
-  useEffect(() => {
-    (async () => {
-      const res = await getAllIncriptions();
-      if (res) {
-        setRows(res);
-      }
-    })(setLoading(false));
-  }, []);
+const rows = [
+  createData(
+    1,
+    "7b",
+    "26.345.816",
+    "DURSO, Fernando J.",
+    "DURSO, Fernando J.",
+    "DURSO, Fernando J",
+    "26.345.816",
+    "FTZ 322",
+    "445",
+    "28/02/2023",
+    "CALEDONIA",
+    "2115693",
+    "10/07/2022",
+    "222317189",
+    "09/07/2022",
+    "M BENZ",
+    "50",
+    "15/05/2022",
+    "23/03/2023",
+    "Sin prorroga",
+    "20-26345816-9",
+    "DURSO, Fernando J",
+    "20-26345816-9",
+    "2006"
+  ),
+];
+
+export default function DataTable() {
+  // var [rows, setRows] = useState([]);
+  // var [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await getAllIncriptions();
+  //     if (res) {
+  //       setRows(res);
+  //     }
+  //   })(setLoading(false));
+  // }, []);
 
   return (
     <Paper elevation={3} sx={{ m: 2 }} style={{ height: 750 }}>
@@ -81,7 +186,7 @@ export default function DataTable() {
             },
           }}
           rows={rows}
-          loading={loading}
+          //loading={loading}
           columns={columns}
           pageSize={25}
           rowsPerPageOptions={[25]}
